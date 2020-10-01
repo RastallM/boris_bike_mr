ef369d7727a1e9cc6ac4b94507863c3b7836ec6c
Meryl Rastall <merylrastall@gmail.com>
2020-10-01 19:51:48 +0100

    "feat: added tests to docking_station_spec and updated bike.rb and docking_statoin.rb"

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,6 +3,15 @@ class DockingStation
     def release_bike
         Bike.new
     end
+
+    def dock(bike)
+        bike
+    end
+      
+    def bike
+    end
+
+
     
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,8 +8,13 @@ describe DockingStation do
 
     it { expect(bike).to be_a Bike }
 
-    it { expect(bike.working?).to eq TRUE }
+    it { expect(bike.working?).to eq true }
 
-
+    it "docks something" do
+        expect(docking_station.dock(bike)).to eq bike
+        # it { is_expected.to respond_to(:dock).with(1).argument }
+    end
+    
+    it { is_expected.to respond_to :bike }
 end
 
