73e1cf579381c49b63f8263538fd1c4150559814
Meryl Rastall <merylrastall@gmail.com>
2020-10-01 15:02:34 +0100

    feat: added tests to docking_station.rb and updated bike.rb and docking_station.rb methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike 
+
     it { is_expected.to respond_to :release_bike }
+
+    it { expect(bike).to be_a Bike }
+
+    it { expect(bike.working?).to eq TRUE }
+
+
 end
 
