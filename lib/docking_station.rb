8f2fed7c379a8425e6c5f953b0fa68cbd864dc83
KC Thomas <thisiskcthomas@gmail.com>
2020-10-02 16:23:54 +0100

    Added private predicate method to check if bike_rack is full. Refactored #release_bike to use .empty? method.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,14 +7,20 @@ class DockingStation
     end
 
     def release_bike
-        fail 'No bikes available' unless bike_rack.count >= 1
+        fail 'No bikes available' if bike_rack.empty?
         bike_rack[0]
     end
 
     def dock(bike)
-        fail 'The station is full!' unless bike_rack.count < 20 
+        fail 'The station is full!' if full?
         bike_rack << bike
     end
-      
+    
+    private
+
+    def full?
+        return true if bike_rack.count == 20
+        false
+    end
 end
 
